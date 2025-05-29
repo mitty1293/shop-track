536a753173b66a11ac4fdf01beebe737161db49d
mitty1293 <k.fmichi@gmail.com>
2025-05-29 13:10:59 +0900

    API側のurlsを整理

--- backend/records/urls.py
+++ backend/records/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework import routers
 
 from .views import (
@@ -12,15 +13,26 @@ from .views import (
 )
 
 router = routers.DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'units', UnitViewSet)
-router.register(r'manufacturers', ManufacturerViewSet)
-router.register(r'origins', OriginViewSet)
-router.register(r'stores', StoreViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'shopping-records', ShoppingRecordViewSet)
+router.register(r"categories", CategoryViewSet)
+router.register(r"units", UnitViewSet)
+router.register(r"manufacturers", ManufacturerViewSet)
+router.register(r"origins", OriginViewSet)
+router.register(r"stores", StoreViewSet)
+router.register(r"products", ProductViewSet)
+router.register(r"shopping-records", ShoppingRecordViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
